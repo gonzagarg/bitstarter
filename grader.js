d433744c09894d15565bdd2072210e3ebb178940
Ruth Gonzaga <gonzagarg@yahoo.com.br>
2013-07-07 22:11:53 +0000

    Grader,js getting url, but not checking yet

--- grader.js
+++ grader.js
@@ -26,6 +26,9 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require('restler');
+var URL_DEFAULT = "http://argile-eyrie-5235.herokuapp.com";
+
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,10 +68,17 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url>', 'URL to index.html')
         .parse(process.argv);
+//    if (program.url) { 
+//	rest.get(program.url).on('complete', function(result){
+//	    console.log(result);
+//	});}
+
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
+
